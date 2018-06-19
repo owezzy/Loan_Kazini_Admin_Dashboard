f1b73ef02e4327ac2e6c3aa46de28c44236f3e5e
Owen Adira <owenobezzy@gmail.com>
2018-06-19 14:56:41 +0300

    updates to deploy to heroku

--- app.js
+++ app.js
@@ -1,46 +1,16 @@
-var express = require("express");
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var bodyParser = require("body-parser");
+//Install express server
+const express = require('express');
+const path = require('path');
 
-var client = require("./routes/client");
-var app = express();
+const app = express();
 
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: "false" }));
-app.use(express.static(path.join(__dirname, "dist")));
-app.use("/clients", express.static(path.join(__dirname, "dist")));
-app.use("/client", client);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-// mongoose setup
-//var mongoose = require("mongoose");
-/mongoose.Promise = require("bluebird");
-mongoose
-  .connect("mongodb://localhost/LoanKazini", {
-    useMongoClient: true,
-    promiseLibrary: require("bluebird")
-  })
-  .then(() => console.log("connection succesful"))
-  .catch(err => console.error(err));
-
-module.exports = app;
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
